eb2308be9601a7497ce50b03fb10472cfd9b4b70
Neetha <roseneethar@gmail.com>
2023-06-09 11:47:42 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
+        <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+
+  
+  return (
+    <header>
+        This is the header
+    </header>
+  )
+}
+
+export default Header
